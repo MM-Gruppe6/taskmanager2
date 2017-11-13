8a779f26caad1ec929ff4dd8077abab32c60c3c7
Stefan Blomberg <steblo94@gmail.com>
2017-11-13 14:08:01 +0100

    oppdatert, server skal fungere

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express(); //server-app
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    
+  //set headers
+  res.set('Access-Control-Allow-Origin', '*'); 
+  res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    
+  //set statusline and body - and send
+  res.status(200).send('Hello World!'); //status-line and body
+    
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, function () {
+  console.log('Server listening on port !');
+});
