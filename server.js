39b382ebc99865ad8cea9e77133e387430404364
Stefan Blomberg <steblo94@gmail.com>
2017-11-14 11:10:30 +0100

    Hello, update incoming

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express(); //server-app
 
+
+
+
 // global for all routes -------------------------
 app.use(function(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
@@ -10,14 +13,16 @@ app.use(function(req, res, next) {
 
 // -----------------------------------------------
 //route handling is delegated to:
-var tasks = require('/tasks.js');
-app.use('/tasks/', tasks);
+
+//Jostein, stemmer dette, evt. hva skal stå her og hvorfor?
+var tasks = require('./tasks.js');
+app.use('taskmanager2/tasks/', tasks);
 
 //Not yet implemented:
 //var users = require('./users.js');
 //app.use('/travellog/users/', users);
 
-//------------------------------------------------
-app.listen(8080, function () {
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
   console.log('Server listening on port 8080!');
 });
