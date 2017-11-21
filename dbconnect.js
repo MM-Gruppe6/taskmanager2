de7ce1b08764cf3c50ff5e3cd566865f31306cb3
katarina Moss Johannesen <katarina-moss@hotmail.com>
2017-11-21 16:28:55 +0100

    tilkobling til heruku

--- dbconnect.js
+++ dbconnect.js
@@ -4,7 +4,9 @@ pgp.pg.defaults.ssl = true;
 
 
 //db connect string
-var db = pgp('postgres://mrkzqengigyvmy:6061543bdd29cacf8345e7c961e76dbed61cd373e553899b5a108abf25493d08@ec2-54-247-123-130.eu-west-1.compute.amazonaws.com:5432/d9hrc499slemoh');
+var conn = process.env.DATABASE_URL || 'postgres://mrkzqengigyvmy:6061543bdd29cacf8345e7c961e76dbed61cd373e553899b5a108abf25493d08@ec2-54-247-123-130.eu-west-1.compute.amazonaws.com:5432/d9hrc499slemoh';
+
+var db = pgp(conn);
 
 
 //export module
